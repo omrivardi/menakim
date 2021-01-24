a767dd645fe765a5dc08306c8f4593dace06fc65
ELK4N4 <elkana.hendler@gmail.com>
2021-01-24 20:26:34 +0200

    fix build

--- src/views/AddProtest.js
+++ src/views/AddProtest.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { observer } from 'mobx-react-lite';
-//import { Link } from 'react-router-dom';
-//import { useTranslation } from 'react-i18next';
+// import { Link } from 'react-router-dom';
+// import { useTranslation } from 'react-i18next';
 import { useStore } from '../stores';
 import { createProtest } from '../api';
 import { SignUp } from '../views';
 import { ProtestForm } from '../components';
-//import { Button, PageWrapper, PageContentWrapper } from '../components';
+// import { Button, PageWrapper, PageContentWrapper } from '../components';
 
 function AddProtest() {
   const store = useStore();
