bd95d17618e4d1cd7de90fd15e498a076f727e0b
ELK4N4 <elkana.hendler@gmail.com>
2021-02-01 22:03:42 +0200

    fixed build

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { observer } from 'mobx-react-lite';
 import { useStore } from '../../stores';
-import { pointWithinRadius } from '../../utils';
+//import { pointWithinRadius } from '../../utils';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 import styled from 'styled-components/macro';
 import L from 'leaflet';
@@ -72,7 +72,7 @@ const balfur = [31.7749837, 35.219797];
 
 function AppMap({ hoveredProtest }) {
   const store = useStore();
-  const { mapStore, protestStore, userCoordinates: coordinates } = store;
+  const { mapStore, userCoordinates: coordinates } = store;
   const addressInputRef = useRef(); // Search Bar ref, used by the combobox
 
   /*
