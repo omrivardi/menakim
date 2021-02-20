1786272897c21b5d1c89ceb6b65797c20635f208
omrivardi <omrivardi@gmail.com>
2021-02-20 22:16:28 +0200

    add tutorialPopup to 18n ns

--- src/i18n.js
+++ src/i18n.js
@@ -19,7 +19,7 @@ i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    ns: ['translations', 'signup', 'addCleanup', 'header', 'card'],
+    ns: ['translations', 'signup', 'addCleanup', 'header', 'card', 'tutorialPopup'],
     defaultNS: 'translations',
     fallbackLng: 'he',
     debug: true,
