ce534277e05b16d5fa3c71e2f7882bbfeb80ddf4
Raz <razrimon27@gmail.com>
2021-02-21 20:13:25 +0200

    Fixed bugs in translation

--- src/i18n.js
+++ src/i18n.js
@@ -7,6 +7,33 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 // have a look at the Quick start guide
 // for passing in lng and translations on init
 
+const options = {
+  // order and from where user language should be detected
+  order: ['querystring', 'cookie', 'localStorage', 'sessionStorage', 'navigator', 'htmlTag', 'path', 'subdomain'],
+
+  // keys or params to lookup language from
+  lookupQuerystring: 'lng',
+  lookupCookie: 'i18next',
+  lookupLocalStorage: 'i18nextLng',
+  lookupSessionStorage: 'i18nextLng',
+  lookupFromPathIndex: 0,
+  lookupFromSubdomainIndex: 0,
+
+  // cache user language on
+  caches: ['localStorage', 'cookie'],
+  excludeCacheFor: ['cimode'], // languages to not persist (cookie, localStorage)
+
+  // optional expire and domain for set cookie
+  cookieMinutes: 10,
+  cookieDomain: 'myDomain',
+
+  // optional htmlTag with lang attribute, the default is:
+  htmlTag: document.documentElement,
+
+  // optional set cookie options, reference:[MDN Set-Cookie docs](https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie)
+  cookieOptions: { path: '/', sameSite: 'strict' },
+};
+
 i18n
   // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
   // learn more: https://github.com/i18next/i18next-http-backend
@@ -19,11 +46,23 @@ i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    ns: ['translations', 'signup', 'addCleanup', 'header', 'card', 'tutorialPopup'],
+    ns: [
+      'translations',
+      'signup',
+      'addCleanup',
+      'header',
+      'card',
+      'tutorialPopup',
+      'donate',
+      'protestList',
+      'tutorialPopup',
+      'utils',
+    ],
     defaultNS: 'translations',
     fallbackLng: 'he',
     debug: true,
-
+    whiteList: ['en', 'ar'],
+    detection: options,
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
