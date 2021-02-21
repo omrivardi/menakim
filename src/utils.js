64be5c2314a633c23126260964e5a3c9d18ad30f
Raz Rimon <razrimon27@gmail.com>
2021-02-21 23:25:41 +0200

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,5 @@
 import isPointWithinRadius from 'geolib/es/isPointWithinRadius';
 import getDistance from 'geolib/es/getDistance';
-import { useTranslation } from 'react-i18next';
 
 export function getCurrentPosition() {
   return new Promise((resolve, reject) => {
@@ -46,7 +45,6 @@ export function calculateDistance(pointA, pointB) {
 }
 
 export function formatDistance(distance) {
-  const { t } = useTranslation('utils');
   if (distance > 1000) {
     return `${(distance / 1000).toFixed(1)} ${t('km')}`;
   } else {
